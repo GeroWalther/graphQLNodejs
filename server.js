e1a54a1dbe52073b7d8233142a4fd05b4d06914c
GeroWalther <gero.walther@gmail.com>
2024-02-17 18:42:55 +0100

    added ruru GraphQL

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-var { graphql, buildSchema } = require('graphql');
+var express = require('express');
+var { createHandler } = require('graphql-http/lib/use/express');
+var { buildSchema } = require('graphql');
+var { ruruHTML } = require('ruru/server');
 
 const AGE = 29;
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String,
+    hello: String
     age: Int
   }
 `);
@@ -19,10 +22,23 @@ var rootValue = {
   },
 };
 
-graphql({
-  schema,
-  source: '{ hello, age }',
-  rootValue,
-}).then((response) => {
-  console.log(response);
+var app = express();
+
+// Create and use the GraphQL handler.
+app.all(
+  '/graphql',
+  createHandler({
+    schema,
+    rootValue,
+  })
+);
+
+// Serve the GraphiQL IDE.
+app.get('/', (_req, res) => {
+  res.type('html');
+  res.end(ruruHTML({ endpoint: '/graphql' }));
 });
+
+// Start the server at port
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
