8ea815290d67cedf0a574a8cdbb5d7378616aa26
GeroWalther <gero.walther@gmail.com>
2024-02-24 19:21:42 +0100

    users crud mongoDB

--- src/graphql/models/user.js
+++ src/graphql/models/user.js
@@ -1,50 +1,68 @@
+import { ObjectId } from 'mongodb';
+
 export const typeDef = /* GraphQL */ `
   type Query {
-    user: User
+    users: [User!]!
+    user(id: ID!): User
   }
 
   type Mutation {
     createUser(user: NewUserInput!): User
+    deleteUser(id: ID!): Boolean
+    updateUser(id: ID!, update: UpdateUserInput): User
   }
 
   input NewUserInput {
     name: String!
-    age: Int!
+    email: String!
+  }
+  input UpdateUserInput {
+    name: String!
   }
 
   type User {
-    id: Int
+    id: ID!
     name: String
-    age: Int
+    email: String
   }
 `;
 
 export const resolvers = {
   Query: {
-    user: () => {
-      return {
-        id: 1,
-        name: 'Gero',
-      };
+    users: (obj, args, { mongo }) => {
+      return mongo.mFlixusers.find().limit(20).toArray();
+    },
+    user: (obj, { id }, { mongo }) => {
+      return mongo.mFlixusers.findOne({ _id: new ObjectId(id) });
     },
   },
 
   Mutation: {
     // obj, args, ctx
     createUser: async (_, { user }, { mongo }) => {
-      console.log(mongo);
-      const movies = await mongo.mFlixmovieCollection.find().toArray();
-      console.log(movies);
-      // insert into db
-
+      const res = await mongo.mFlixusers.insertOne(user);
       return {
-        id: 1,
+        id: res.insertedId,
         ...user,
       };
+    },
+    deleteUser: async (_, { id }, { mongo }) => {
+      await mongo.mFlixusers.deleteOne({ _id: new ObjectId(id) });
+      return true;
+    },
+    updateUser: async (obj, { id, update }, { mongo }) => {
+      const res = await mongo.mFlixusers.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { name: update.name } }
+      );
+      return mongo.mFlixusers.findOne({ _id: new ObjectId(id) });
     },
   },
 
   User: {
+    id: (obj) => {
+      return obj._id || obj.id;
+    },
     name: (obj) => {
       return obj.name.toUpperCase();
     },
